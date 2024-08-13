14de65cb8c8a6cd0c3da01430b3122d62c900394
jdr14 <jdr14@vt.edu>
2024-08-13 01:46:18 -0400

    Comma separated data entry logged locally now.

--- server.cpp
+++ server.cpp
@@ -1,36 +1,34 @@
 #define CROW_MAIN
-#include "../Crow/include/crow.h"
+#include <crow.h>
 #include <iostream>
 #include <string>
-// #include <crow/json/json.h>
 
-int main()
-{
+using namespace crow;
+using namespace std;
+
+int main() {
     crow::SimpleApp app;
+    app.loglevel(crow::LogLevel::Debug);
 
-    // Define a POST endpoint
-    CROW_ROUTE(app, "/api/endpoint").methods(crow::HTTPMethod::POST)
+    // POST endpoint for adding recipe
+    CROW_ROUTE(app, "/api/add-recipe").methods(crow::HTTPMethod::POST)
     ([](const crow::request& req) {
         auto data = crow::json::load(req.body);
         if (!data) {
             return crow::response(400, "Invalid JSON");
         }
 
-        std::string key1 = data["key1"].s();
-        std::string key2 = data["key2"].s();
+        std::string recipe_name = data["recipe"].s();
+        std::string ingredients_list = data["ingredients"].s();
 
         crow::json::wvalue response;
         response["message"] = "Data received successfully";
-        response["receivedData"]["key1"] = key1;
-        response["receivedData"]["key2"] = key2;
+        response["receivedData"]["recipe"] = recipe_name;
+        response["receivedData"]["ingredients"] = ingredients_list;
+        std::cout << "Recipe received: " << recipe_name << std::endl;
 
         return crow::response(response);
     });
 
     app.port(4000).multithreaded().run();
 }
-
-
-// wget https://github.com/CrowCpp/Crow/releases/download/v0.3%2B1/crow_all.h
-// Mac: brew install boost
-// To Compile:  g++ -o server server.cpp -lboost_system -lpthread
--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const path = require('path');
-
 const app = express();
 const PORT = 4000;
+
+// Middleware to parse JSON bodies
+app.use(express.json());
 
 // Serve static files from the "public" directory
 app.use(express.static(path.join(__dirname, 'public')));
@@ -11,15 +13,17 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-// Define an API endpoint
-app.post('/api/endpoint', (req, res) => {
-    const data = req.body;
-    console.log('Received data:', data);
+// Handle submission of a recipe
+app.post('/api/add-recipe', (req, res) => {
+    const { recipe, ingredients } = req.body;
+    console.log('Received recipe:', recipe);
+    console.log('Received ingredients:', ingredients); // This should be an array
 
     // Send a response back to the front end
     res.json({
-        message: 'Data received successfully',
-        receivedData: data
+        message: 'Recipe received successfully',
+        receivedRecipe: recipe,
+        receivedIngredients: ingredients
     });
 });
 
